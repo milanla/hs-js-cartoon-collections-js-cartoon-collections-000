064f3c5856d876b1a37455d049c36bb04e3aaa8e
Milan Sacher <milan.lanlada@gmail.com>
2018-07-23 20:14:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var name = "";
+  for (let i=0; i < dwarves.length; i++) {
+    name += (i+1) + ". " + dwarves[i] + " ";
+  }
+  return name;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var shout = [];
+  for (let i=0; i < planeteerCalls.length; i++) {
+  shout.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+  return shout;
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words) { 
+  for (let i=0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i=0; i < foods.length; i++) {
+    if (foods[i] === 'cheddar' ||
+        foods[i] === 'gouda' ||
+        foods[i] === 'camembert') {
+          return foods[i];
+        }
+  }
+  return 'no cheese!';
 }
